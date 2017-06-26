a0436e5626a37daa20b26a53f2e87993232824d6
LastExceed <chris20194@googlemail.com>
2017-06-26 19:11:21 +0200

    update form

--- Bridge/Form1.Designer.cs
+++ Bridge/Form1.Designer.cs
@@ -37,8 +37,12 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.buttonLogin = new System.Windows.Forms.Button();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.labelIP = new System.Windows.Forms.Label();
+            this.groupBoxServer = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.labelPort = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
+            this.groupBoxServer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // richTextBoxChat
@@ -48,7 +52,7 @@
             this.richTextBoxChat.ForeColor = System.Drawing.Color.White;
             this.richTextBoxChat.Location = new System.Drawing.Point(238, 0);
             this.richTextBoxChat.Name = "richTextBoxChat";
-            this.richTextBoxChat.Size = new System.Drawing.Size(264, 164);
+            this.richTextBoxChat.Size = new System.Drawing.Size(258, 218);
             this.richTextBoxChat.TabIndex = 1;
             this.richTextBoxChat.Text = "richTextBoxChat";
             // 
@@ -58,7 +62,7 @@
             this.listBoxPlayers.FormattingEnabled = true;
             this.listBoxPlayers.Location = new System.Drawing.Point(118, 0);
             this.listBoxPlayers.Name = "listBoxPlayers";
-            this.listBoxPlayers.Size = new System.Drawing.Size(120, 164);
+            this.listBoxPlayers.Size = new System.Drawing.Size(120, 218);
             this.listBoxPlayers.TabIndex = 2;
             // 
             // textBox1
@@ -72,9 +76,9 @@
             // 
             // textBoxIP
             // 
-            this.textBoxIP.Location = new System.Drawing.Point(7, 16);
+            this.textBoxIP.Location = new System.Drawing.Point(6, 18);
             this.textBoxIP.Name = "textBoxIP";
-            this.textBoxIP.Size = new System.Drawing.Size(100, 20);
+            this.textBoxIP.Size = new System.Drawing.Size(91, 20);
             this.textBoxIP.TabIndex = 4;
             this.textBoxIP.Text = "exceed.rocks";
             // 
@@ -88,7 +92,7 @@
             // 
             // buttonInfo
             // 
-            this.buttonInfo.Location = new System.Drawing.Point(18, 136);
+            this.buttonInfo.Location = new System.Drawing.Point(20, 179);
             this.buttonInfo.Name = "buttonInfo";
             this.buttonInfo.Size = new System.Drawing.Size(75, 23);
             this.buttonInfo.TabIndex = 6;
@@ -102,7 +106,7 @@
             this.groupBox1.Controls.Add(this.linkLabel1);
             this.groupBox1.Controls.Add(this.textBox3);
             this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Location = new System.Drawing.Point(1, 38);
+            this.groupBox1.Location = new System.Drawing.Point(-1, 79);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(113, 94);
             this.groupBox1.TabIndex = 7;
@@ -128,32 +132,70 @@
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "register";
             // 
-            // labelIP
+            // groupBoxServer
             // 
-            this.labelIP.AutoSize = true;
-            this.labelIP.Location = new System.Drawing.Point(7, 0);
-            this.labelIP.Name = "labelIP";
-            this.labelIP.Size = new System.Drawing.Size(52, 13);
-            this.labelIP.TabIndex = 8;
-            this.labelIP.Text = "server IP:";
+            this.groupBoxServer.Controls.Add(this.labelPort);
+            this.groupBoxServer.Controls.Add(this.numericUpDown1);
+            this.groupBoxServer.Controls.Add(this.textBoxIP);
+            this.groupBoxServer.Location = new System.Drawing.Point(6, 0);
+            this.groupBoxServer.Name = "groupBoxServer";
+            this.groupBoxServer.Size = new System.Drawing.Size(106, 73);
+            this.groupBoxServer.TabIndex = 8;
+            this.groupBoxServer.TabStop = false;
+            this.groupBoxServer.Text = "Server";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(40, 41);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            49152,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(57, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            12345,
+            0,
+            0,
+            0});
+            // 
+            // labelPort
+            // 
+            this.labelPort.AutoSize = true;
+            this.labelPort.Location = new System.Drawing.Point(11, 44);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(28, 13);
+            this.labelPort.TabIndex = 6;
+            this.labelPort.Text = "port:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 164);
-            this.Controls.Add(this.labelIP);
+            this.ClientSize = new System.Drawing.Size(496, 218);
+            this.Controls.Add(this.groupBoxServer);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.buttonInfo);
-            this.Controls.Add(this.textBoxIP);
             this.Controls.Add(this.listBoxPlayers);
             this.Controls.Add(this.richTextBoxChat);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(512, 9999);
+            this.MinimumSize = new System.Drawing.Size(512, 256);
             this.Name = "Form1";
             this.Text = "fd";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBoxServer.ResumeLayout(false);
+            this.groupBoxServer.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -168,7 +210,9 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button buttonLogin;
         private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.Label labelIP;
+        private System.Windows.Forms.GroupBox groupBoxServer;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
 
