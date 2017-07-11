4a32936ce0e817b8d15fb5e84a805ad37880c7e2
Redcrafter <redcrafter@hotmail.de>
2017-07-11 17:49:06 +0200

    DB fix

--- Server/users.cs
+++ Server/users.cs
@@ -14,6 +14,7 @@ namespace Server
     
     public partial class users
     {
+        public int index { get; set; }
         public string username { get; set; }
         public string Email { get; set; }
         public string hash { get; set; }
