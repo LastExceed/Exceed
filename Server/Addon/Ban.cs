3b47fff9bf2cc47a8f406b8cd04d1680aedfe777
Redcrafter <redcrafter@hotmail.de>
2017-11-17 18:16:00 +0100

    Added Ban class

---
+++ Server/Addon/Ban.cs
@@ -0,0 +1,15 @@
+﻿namespace Server.Addon {
+    public class Ban {
+        public string Name { get; set; }
+        public string Ip { get; set; }
+        public string Mac { get; set; }
+        public string Reason { get; set; }
+
+        public Ban(string name, string ip, string mac, string reason = "ban_message_default") {
+            Name = name;
+            Ip = ip;
+            Mac = mac;
+            Reason = reason;
+        }
+    }
+}
