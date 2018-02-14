1ce2d7352782624bf5961818b029729d3747a1a6
chris beck <chris20194@googlemail.com>
2018-02-14 15:16:43 +0100

    initial implementation of knockback

--- Resources/Database.cs
+++ Resources/Database.cs
@@ -1,6 +1,8 @@
 ﻿namespace Resources {
     static public class Database {
         public const int mapseed = 8710; //hardcoded for now
-        public const int bridgeVersion = 11;
+        public const int bridgeVersion = 12;
+        public const string serverIP = "localhost";//temp
+        public const int serverPort = 12346;
     }
 }
