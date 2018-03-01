866f11939a4579d92577e610bc7dbf432deedf48
davidlkang <david.kang@gmx.de>
2018-03-01 18:51:34 +0100

    First Implementation of LiveMap

--- Bridge/FormMap.cs
+++ Bridge/FormMap.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace Bridge {
     public partial class FormMap : Form {
         public FormMap() {
             InitializeComponent();
+        }
+        List<Label> playercollection = new List<Label>();
+        private void Refreshtimer_Tick(object sender, EventArgs e) {
+            foreach (Label label in playercollection) {
+                Controls.Remove(label);
+            }
+            playercollection.Clear();
+            foreach (var entity in BridgeTCPUDP.dynamicEntities.Values) {
+                if (entity.hostility == Resources.Hostility.Player) {
+                    Label playerlabel = new Label {
+                        Left = (int)entity.position.x / 0x10000,
+                        Top = (int)entity.position.y / 0x10000,
+                        Text = entity.name,
+                        AutoSize = true,
+                    };
+                    Controls.Add(playerlabel);
+                    playercollection.Add(playerlabel);
+                }
+            }
         }
     }
 }
