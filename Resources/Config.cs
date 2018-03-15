dd9c8c35a78e011cfff4b92cf796ec59e58e3e9e
LastExceed <chris20194@googlemail.com>
2018-03-15 22:54:03 +0100

    update submodule

--- Resources/Config.cs
+++ Resources/Config.cs
@@ -2,7 +2,7 @@
     static public class Config {
         public const int mapseed = 8710; //hardcoded for now
         public const int bridgeVersion = 13;
-        public const string serverIP = "localhost";//temp
+        public const string serverIP = "92.206.52.235";//temp
         public const int serverPort = 12346;
     }
 }
