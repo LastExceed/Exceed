8916fdcf32770478057c27af18431656df6f996d
LastExceed <chris20194@googlemail.com>
2018-10-14 14:29:06 +0200

    add dmgReductionByShield

--- Server/Extensions/Balancing.cs
+++ Server/Extensions/Balancing.cs
@@ -11,9 +11,21 @@ namespace Server.Extensions {
             if (attack.Damage > 0) {
                 attack.Damage *= 0.5f; //dmg
             }
-            else {
-                attack.Damage *= 0.333333f; //heal
+            //else {
+            //    attack.Damage *= 0.333333f; //heal
+            //}
+            var target = Server.dynamicEntities[attack.Target];
+            var dmgReductionByShield = 0f;
+            if (target.equipment[(int)Equipment.LeftWeapon].subtype == (byte)ItemSubtypeWeapon.Shield) {
+                dmgReductionByShield += 0.25f;
             }
+            if (target.equipment[(int)Equipment.RightWeapon].subtype == (byte)ItemSubtypeWeapon.Shield) {
+                dmgReductionByShield += 0.25f;
+            }
+            if (attack.Damage > 0) {
+                attack.Damage *= 1 - dmgReductionByShield;
+            }
+            Log.PrintLn(player.entity.name + " " + attack.Target + " " + attack.Damage + " " + attack.Critical, System.ConsoleColor.Magenta);
             if (attack.Target == player.entity.guid) {//players can't damage themselves. this prevents double self heals since selfheal is already applied locally
                 attack.Damage = 0;
             }
