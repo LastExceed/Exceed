ecb4bee1a4e66ec1a5f60587c40259b4c4fa8c6f
LastExceed <chris20194@googlemail.com>
2022-04-08 16:57:51 +0200

    setup manual deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	kotlin("jvm") version "1.6.20"
+	application
 }
 group = "com.github.lastexceed"
 version = "1.0-SNAPSHOT"
@@ -13,4 +14,8 @@ repositories {
 dependencies {
 	implementation("io.ktor", "ktor-network", "2.0.+")
 	implementation("com.github.lastexceed", "CubeworldNetworking", "1.3.51")
+}
+
+application {
+	mainClass.set("MainKt")
 }
