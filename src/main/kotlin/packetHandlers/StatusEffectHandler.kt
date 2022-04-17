4b3739ea4d76c304d517dbf4051c68ce6adae28b
LastExceed <chris20194@googlemail.com>
2022-04-17 09:24:42 +0200

    clean up StatusEffectHandler

--- src/main/kotlin/packetHandlers/StatusEffectHandler.kt
+++ src/main/kotlin/packetHandlers/StatusEffectHandler.kt
@@ -1,34 +1,42 @@
 package packetHandlers
 
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import com.github.lastexceed.cubeworldnetworking.packets.*
 import com.github.lastexceed.cubeworldnetworking.utils.*
 import Player
-import kotlinx.coroutines.Dispatchers
+import modules.Balancing
 
 object StatusEffectHandler : PacketHandler<StatusEffect> {
 	override suspend fun handlePacket(packet: StatusEffect, source: Player) {
-		val soundList =
-			if (packet.type == StatusEffect.Type.Poison) {
-				applyPoison(source, packet)
-
-				listOf(
-					SoundEffect(
-						Utils.creatureToSoundPosition(source.character.position),
-						SoundEffect.Sound.FireTrap
+		when (val type = packet.type) {
+			StatusEffect.Type.Poison -> {
+				applyPoisonDamageTicks(source, packet)
+				WorldUpdate(
+					statusEffects = listOf(packet),
+					soundEffects = listOf(
+						SoundEffect(
+							Utils.creatureToSoundPosition(source.character.position),
+							SoundEffect.Sound.FireTrap
+						)
 					)
 				)
-			} else emptyList()
-		val miscellaneous = WorldUpdate(
-			soundEffects = soundList,
-			statusEffects = listOf(packet)
-		)
-		source.layer.broadcast(miscellaneous, source)
+			}
+			StatusEffect.Type.WarFrenzy,
+			StatusEffect.Type.Bulwalk,
+			StatusEffect.Type.Camouflage,
+			StatusEffect.Type.ManaShield,
+			StatusEffect.Type.FireSpark,
+			StatusEffect.Type.Intuition,
+			StatusEffect.Type.Elusiveness,
+			StatusEffect.Type.Swiftness -> {}
+
+			else -> error("unknown status effect type $type")
+		}
+		source.layer.broadcast(WorldUpdate(statusEffects = listOf(packet)), source)
 	}
 
-	private fun applyPoison(source: Player, statusEffect: StatusEffect) {
+	private fun applyPoisonDamageTicks(source: Player, statusEffect: StatusEffect) {
 		//todo: support non-players
 		val targetPlayer = source.layer.players[statusEffect.target] ?: return
 		targetPlayer.scope.launch {
