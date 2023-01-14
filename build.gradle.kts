606dafbc7239b511c38dc0ea7ad7fba2a6f41f02
LastExceed <chris20194@googlemail.com>
2023-01-14 10:19:20 +0100

    add gradle config for deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
 	kotlin("jvm") version "1.6.20"
 	application
@@ -15,8 +17,17 @@ dependencies {
 	implementation("io.ktor", "ktor-network", "2.0.+")
 	implementation("com.github.lastexceed", "CubeworldNetworking", "1.4.0")
 	implementation("com.github.ziggy42", "kolor", "1.0.0")
+	implementation(kotlin("stdlib-jdk8"))
 }
 
 application {
 	mainClass.set("MainKt")
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+	jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+	jvmTarget = "1.8"
 }
