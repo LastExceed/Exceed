e335f1dbd0bacd8e1bea3bae31660fbb99eabb7b
LastExceed <chris20194@googlemail.com>
2023-01-17 09:52:14 +0100

    some debugging commands

--- src/main/kotlin/modules/ExperimentalStuff.kt
+++ src/main/kotlin/modules/ExperimentalStuff.kt
@@ -28,33 +28,82 @@ fun registerCommands() {
 		)
 		val miscellaneous = WorldUpdate(pickups = listOf(pickup, pickup, pickup, pickup))
 
-		({ caller.send(miscellaneous) })
+		({ caller.layer.broadcast(miscellaneous) })
 	}
 
-	ChatCommands.commands["reload"] = Command { caller, parameters ->
-		{
-			caller.clearCreatures()
-			caller.layer.creatures.forEach { caller.send(it.value.toCreatureUpdate()) }
-		}
+	ChatCommands.commands["sfx"] = Command { caller, parameters ->
+		val sound = SoundEffect(
+			position = Utils.creatureToSoundPosition(caller.character.position),
+			sound = SoundEffect.Sound.values[parameters.next().toInt()]
+		)
+
+		val worldUpdate = WorldUpdate(soundEffects = listOf(sound))
+
+		({ caller.send(worldUpdate) })
+	}
+
+	ChatCommands.commands["army"] = Command { caller, parameters ->
+		({
+			(2302..2302).forEach {
+				caller.send(
+					caller.character.copy(
+						id = CreatureId(2L),
+						//position = caller.character.position.copy(z = caller.character.position.z + 0x100000),
+						appearance = Appearance(
+							unknownA = 0,
+							unknownB = 0,
+							hairColor = Vector3(255.toByte(),255.toByte(),255.toByte()),
+							unknownC = 0,
+							flags = FlagSet<AppearanceFlag>(BooleanArray(16)).apply { this[AppearanceFlag.LockedInPlace] = true },
+							creatureSize = Vector3(10f,10f,10f),
+							headModel = it.toShort(),
+							hairModel = it.toShort(),
+							handModel = it.toShort(),
+							footModel = it.toShort(),
+							bodyModel = it.toShort(),
+							tailModel = it.toShort(),
+							shoulder2Model = it.toShort(),
+							wingModel = it.toShort(),
+							headSize = 10f,
+							bodySize = 10f,
+							handSize = 10f,
+							footSize = 10f,
+							shoulder2Size = 10f,
+							weaponSize = 10f,
+							tailSize = 10f,
+							shoulder1Size = 10f,
+							wingSize = 10f,
+							bodyRotation = 0f,
+							handRotation = Vector3(0f, 0f,0f),
+							feetRotation = 0f,
+							wingRotation = 0f,
+							tailRotation = 0f,
+							bodyOffset = Vector3(0f, 0f, 0f),
+							headOffset = Vector3(0f, 0f, 0f),
+							handOffset = Vector3(0f, 0f, 0f),
+							footOffset = Vector3(0f, 0f, 0f),
+							tailOffset = Vector3(0f, 0f, 0f),
+							wingOffset = Vector3(0f, 0f, 0f),
+						),
+						name = it.toString()
+					).toCreatureUpdate()
+				)
+			}
+		})
 	}
 
 	ChatCommands.commands["speed"] = Command(true) { caller, parameters ->
-		val camo = StatusEffect(
-			caller.character.id,
-			caller.character.id,
-			StatusEffect.Type.Camouflage,
-			modifier = 99999f,
-			duration = 10_000,
-			creatureId3 = caller.character.id
-		)
-		val speed = StatusEffect(
-			caller.character.id,
-			caller.character.id,
-			StatusEffect.Type.Swiftness,
-			modifier = 99999f,
-			duration = 10_000,
-			creatureId3 = caller.character.id
-		)
+		val statusEffects =
+			StatusEffect.Type.values.map {
+				StatusEffect(
+					caller.character.id,
+					caller.character.id,
+					it,
+					modifier = 1f,
+					duration = 10_000,
+					creatureId3 = caller.character.id
+				)
+			}
 
 		({ caller.send(WorldUpdate(statusEffects = listOf(speed, camo))) })
 	}
