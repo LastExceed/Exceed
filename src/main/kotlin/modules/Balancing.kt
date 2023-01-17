b3eec8b3ccc64d9ca4674a1c4379efa059c7f670
LastExceed <chris20194@googlemail.com>
2023-01-17 09:49:45 +0100

    class specific damage balancing

--- src/main/kotlin/modules/Balancing.kt
+++ src/main/kotlin/modules/Balancing.kt
@@ -5,8 +5,9 @@ import Player
 import com.github.lastexceed.cubeworldnetworking.packets.*
 
 object Balancing {
-	private const val GLOBAL_DAMAGE_REDUCTION = 0.5f
-	private const val SHIELD_DEFENSE = 0.25f
+	private const val GLOBAL_STUN_MODIFIER = 0.75
+	const val GLOBAL_DAMAGE_REDUCTION_PERCENT = 0.5f
+	private const val SHIELD_DEFENSE_PERCENT = 0.25f
 
 	fun warFrenzyBuff(packet: StatusEffect, source: Player) {
 		source.layer.broadcast(
@@ -19,22 +20,98 @@ object Balancing {
 		)
 	}
 
-	fun adjustDamage(hit: Hit, target: Creature): Hit {
-		val gearDefense = listOf(
+	fun adjustDamage(hit: Hit, source: Creature, target: Creature): Hit {
+		if (hit.damage < 0f) { //heal
+			val healMultiplier =
+				if (hit.target == source.id) {
+					0.5f - 1f//self-heals are applied client side as well (bug), so we need to subtract the vanilla amount
+				} else
+					0.3f
+			return hit.copy(damage = hit.damage * healMultiplier)
+		}
+
+
+		val weapon = source.equipment.rightWeapon
+		val (weaponOffenseAdjustPercent, weaponStunBonus) = when (weapon.typeMajor) {
+			Item.Type.Major.Weapon -> {
+				when (weapon.typeMinor) {
+					Item.Type.Minor.Weapon.Wand -> 0.0f to 0.25
+					Item.Type.Minor.Weapon.Staff -> 0.1f to 0.0
+					Item.Type.Minor.Weapon.Fist -> 0.2f to 0.25
+					else -> 0.0f to 0.0
+				}
+			}
+			else -> 0.0f to 0.0
+		}
+
+		val (classOffenseAdjustPercent, classStunBonus) = when (source.combatClassMajor) {
+			CombatClassMajor.Warrior -> {
+				when (source.combatClassMinor) {
+					CombatClassMinor.Warrior.Berserker -> 0.2f to 0.5
+					else -> 0.0f to 0.0
+				}
+			}
+			CombatClassMajor.Ranger -> {
+				0.1f to when (source.combatClassMinor) {
+					CombatClassMinor.Ranger.Sniper -> 0.25
+					else -> 0.0
+				}
+			}
+			CombatClassMajor.Rogue -> {
+				when (source.combatClassMinor) {
+					CombatClassMinor.Rogue.Assassin -> 0.5f to 0.0
+					else -> -0.1f to 0.0
+				}
+			}
+			CombatClassMajor.Mage -> {
+				when (source.combatClassMinor) {
+					CombatClassMinor.Mage.Fire -> -0.2f to 0.0
+					else -> 0.0f to 0.0
+				}
+			}
+			else -> error("unreachable")
+		}
+
+		val gearDefensePercent = listOf(
 			target.equipment.leftWeapon,
 			target.equipment.rightWeapon
 		).fold(0f) { accumulator, item ->
-			accumulator + if (item.isShield()) SHIELD_DEFENSE else 0f
+			accumulator + if (item.isShield()) SHIELD_DEFENSE_PERCENT else 0f
 		}
 
-		val effectiveDamage = listOf(
-			GLOBAL_DAMAGE_REDUCTION,
-			gearDefense
-		).fold(hit.damage) { accumulator, multiplicativeDefenseModifier ->
+		val totalDefensiveMultiplier = listOf(
+			GLOBAL_DAMAGE_REDUCTION_PERCENT,
+			gearDefensePercent
+		).fold(1f) { accumulator, multiplicativeDefenseModifier ->
 			accumulator * (1f - multiplicativeDefenseModifier)
 		}
 
-		return hit.copy(damage = effectiveDamage)
+		val totalOffensiveMultiplier = listOf(
+			weaponOffenseAdjustPercent,
+			classOffenseAdjustPercent,
+		).fold(1f) { accumulator, damageAdjustPercent ->
+			accumulator * (1f + damageAdjustPercent)
+		}
+
+		val effectiveDamage = listOf(
+			totalDefensiveMultiplier,
+			totalOffensiveMultiplier
+		).fold(hit.damage) { accumulator, damageMultiplier ->
+			accumulator * damageMultiplier
+		}
+
+		val effectiveStunDuration = listOf(
+			GLOBAL_STUN_MODIFIER,
+			1.0 + classStunBonus,
+			1.0 + weaponStunBonus
+		).fold(hit.stuntime) { accumulator, modifier ->
+			(accumulator * modifier).toInt()
+		}
+
+		return hit.copy(
+			damage = effectiveDamage,
+			stuntime = effectiveStunDuration
+		)
 	}
 
 	//add bleeding to make daggers viable
