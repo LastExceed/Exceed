a5b9eb3f9bd537a051d6ce018d8ced4122d480db
LastExceed <chris20194@googlemail.com>
2023-01-18 19:36:21 +0100

    add config file for warps

--- src/main/kotlin/modules/Warps.kt
+++ src/main/kotlin/modules/Warps.kt
@@ -7,32 +7,41 @@ import java.io.File
 import kotlin.math.*
 
 object Warps {
+	val vanillaSpawn = 0x8020800000L to 0x8020800000L
+
 	val locations = File("warps").let {
 		if (it.createNewFile()) {
-			""
+			val defaultContent = "vanilla-spawn;${vanillaSpawn.first};${vanillaSpawn.second}"
+			it.writeText(defaultContent)
+			defaultContent
 		} else it.readText()
 	}.lines()
-		.map { it.split(";") }
+		.map {
+			it.split(";")
+				.let {
+					it[0] to (it[1].toLong() to it[2].toLong())
+				}
+		}.toMap()
 
-	val trueSpawn = Vector3(
-		0x8020800000L,
-		0x8020800000L,
-		0L
-	)
-	val customSpawn = Vector3(
-		549310656995L,
-		551632558116L,
-		0L
-	)
+	private val customSpawn = locations["spawn"]!!.let { (x,y) -> Vector3(x,y,0) }
 
 	init {
-		ChatCommands.commands["spawn"] = Command { caller, parameters ->
+		val warpCommand = Command { caller, parameters ->
+			val destination = parameters.nextOrNull()
+				?: return@Command ({ caller.notify("available warps: " + locations.keys.joinToString()) })
+
+			val (x, y) = locations[destination]
+				?: return@Command ({ caller.notify("unknown destination") })
 			{
 				ExperimentalStuff.teleport(
 					caller,
-					customSpawn
+					Vector3(x, y, 0)
 				)
 			}
+		}
+		ChatCommands.commands["warp"] = warpCommand
+		ChatCommands.commands["spawn"] = Command { caller, parameters ->
+			warpCommand.evaluate(caller, listOf("spawn").listIterator())
 		}
 	}
 
@@ -46,13 +55,14 @@ object Warps {
 	suspend fun onCreatureUpdate(source: Player, packet: CreatureUpdate) {
 		val distanceFromSpawn = packet.position?.let {
 			listOf(
-				it.x - trueSpawn.x,
-				it.y - trueSpawn.y,
-				it.z - trueSpawn.z,
-			).map { it.toDouble().pow(2) }.sum().let { sqrt(it) }
+				it.x - vanillaSpawn.first,
+				it.y - vanillaSpawn.second
+			).map { it.toDouble().pow(2) }
+				.sum()
+				.let(::sqrt)
 		} ?: return
 
-		if (distanceFromSpawn < 1000 * Utils.SIZE_BLOCK) {
+		if (distanceFromSpawn < 5000 * Utils.SIZE_BLOCK) {
 			ExperimentalStuff.teleport(source, customSpawn)
 		}
 	}
